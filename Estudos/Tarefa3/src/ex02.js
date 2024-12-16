c05d986d714217a4b4aaf220b1633df4055601f2
PradoPedro1917 <pedrohenriquepradodenovaes@gmail.com>
2024-12-16 11:43:24 -0300

    Delete Estudos/Tarefa3/src directory

--- Estudos/Tarefa3/src/ex02.js
+++
@@ -1,8 +0,0 @@
-a = prompt("Entre com o menor valor")
-a = parseInt(a)
-b = prompt("Entre com o maior valor")
-b = parseInt(b)
-while (a <= b){
-    console.log(a)
-    a = a + 1
-}
--- Estudos/Tarefa3/src/ex05.js
+++
@@ -1,9 +0,0 @@
-cont = 0
-soma = 0 
-while (cont < 5){
-    numero = prompt("Entre com um numero")
-    numero = parseInt(numero)
-    soma = soma + numero
-    cont = cont + 1
-}
-console.log("somatória", soma)
